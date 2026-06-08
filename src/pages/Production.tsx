64cef7112084a027f99d215c439b9d484dec7f72
Jolab255 <yonahmatete@gmail.com>
2026-06-08 16:55:39 +0300

    feat(audio): use weka alama soundtrack.mp3 for classical audio demo

--- src/pages/Production.tsx
+++ src/pages/Production.tsx
@@ -12,7 +12,7 @@ import classicalImg from '../assets/classical-instrumental.png';
 import orchestralImg from '../assets/orchestral-instrumental.png';
 import instrumentalCreationImg from '../assets/instrumental-creation.webp';
 import simpleInstrumentalAudio from '../assets/simple-instrumental.mp3';
-import classicalInstrumentalAudio from '../assets/classical-instrumental.mp3';
+import classicalInstrumentalAudio from '../assets/weka alama soundtrack.mp3';
 import orchestralInstrumentalAudio from '../assets/ochesrtal-instrumental.mp3';
 import audioRecordingImg from '../assets/audio-recording.webp';
 import audioEditingImg from '../assets/audio-editing.webp';
